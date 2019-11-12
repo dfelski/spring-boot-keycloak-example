d4fcb0a109b23c02436c738ed9737982ade9dd78
Darius Felski <felski@encoway.de>
2019-11-12 13:09:39 +0100

    Spring Security added, configuration adjusted

--- src/main/java/org/example/MyController.java
+++ src/main/java/org/example/MyController.java
@@ -1,10 +1,20 @@
 package org.example;
 
+import org.keycloak.KeycloakPrincipal;
+import org.keycloak.KeycloakSecurityContext;
+import org.keycloak.adapters.springsecurity.token.KeycloakAuthenticationToken;
+import org.keycloak.representations.AccessToken;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 class MyController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(MyController.class);
 
     @GetMapping("/")
     String index(){
@@ -12,7 +22,26 @@ class MyController {
     }
 
     @GetMapping("/secured")
-    String secure(){
+    String secure(HttpServletRequest request){
+
+        KeycloakAuthenticationToken token = (KeycloakAuthenticationToken) request.getUserPrincipal();
+        KeycloakPrincipal principal=(KeycloakPrincipal) token.getPrincipal();
+        KeycloakSecurityContext session = principal.getKeycloakSecurityContext();
+        AccessToken accessToken = session.getToken();
+        String a = principal.getName();
+
+        LOGGER.info("username: {}", accessToken.getPreferredUsername());
+        LOGGER.info("emailId: {}", accessToken.getEmail());
+        LOGGER.info("lastname: {}",accessToken.getFamilyName());
+        LOGGER.info("firstname: {}", accessToken.getGivenName());
+        LOGGER.info("realmName: {}", accessToken.getIssuer());
+
         return "secured";
+    }
+
+    @GetMapping("/logout")
+    String logout(HttpServletRequest httpServletRequest) throws Exception{
+        httpServletRequest.logout();
+        return "index";
     }
 }
